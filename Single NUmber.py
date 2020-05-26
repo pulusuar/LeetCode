9ce410c45bb3f3e2a6cd0fa1989b91c28a689871
Avinash Pulusu <pulusuar@mail.uc.edu>
2020-05-26 13:26:25 -0400

    Add files via upload

---
+++ Single NUmber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(0, len(nums)-1, 2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+        return nums[-1]
